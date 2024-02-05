9cec613601efa64e7b84c36c4f2f6c32bbed224e
BallinExpert <you@example.com>
2024-02-05 16:01:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+const display = document.getElementById("display")
+
+function clearDisplay(){
+    display.value=""
+   
+
+}
+
+function calculate(){
+
+    try{
+        display.value=eval(display.value)
+
+    }catch(error){
+        display.value="Error"
+    }
+    
+
+}
+
+function appendToDisplay(input){
+    
+    display.value +=input
+
+
+}
